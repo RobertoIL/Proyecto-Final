2468ec965ecb2d391d6623c53a50712aa7d4f960
Roberto Illesca <robertillesca1@gmail.com>
2022-06-16 17:23:40 -0400

    ...

--- src/main/java/Cliente/MainCliente.java
+++ src/main/java/Cliente/MainCliente.java
@@ -5,13 +5,4 @@ import java.io.IOException;
 
 
 public class MainCliente{
-
-
-    public static void main(String[] args) throws IOException{
-        Cliente cli = new Cliente(); //Se crea el cliente
-
-        System.out.println("Iniciando cliente\n");
-        cli.startClient(); //Se inicia el cliente
-
-    }
 }
